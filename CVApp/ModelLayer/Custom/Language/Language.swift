80a99c6abd35838518806bc9de9f7bdbc29834f7
Oleksandr Orlov <luximetr@gmail.com>
2020-04-17 10:51:28 +0800

    Created Language, LanguagesFactory, AppearancesFactory.

---
+++ CVApp/ModelLayer/Custom/Language/Language.swift
@@ -0,0 +1,14 @@
+//
+//  Language.swift
+//  CVApp
+//
+//  Created by Oleksandr Orlov on 17/4/20.
+//  Copyright © 2020 Oleksandr Orlov. All rights reserved.
+//
+
+import Foundation
+
+struct Language {
+  let iso639_1Code: String
+  let nativeName: String
+}
---
+++ CVApp/ModelLayer/Custom/Language/LanguagesFactory.swift
@@ -0,0 +1,14 @@
+//
+//  LanguagesFactory.swift
+//  CVApp
+//
+//  Created by Oleksandr Orlov on 17/4/20.
+//  Copyright © 2020 Oleksandr Orlov. All rights reserved.
+//
+
+import Foundation
+
+class LanguagesFactory {
+  
+  
+}
